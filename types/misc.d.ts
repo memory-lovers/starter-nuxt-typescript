4bf30019e56c0b76a9a8f55cea846e79d551fa99
Y.Fujiura <memory.loverz@gmail.com>
2020-05-19 15:37:33 +0900

    introduce HeadMixin

---
+++ types/misc.d.ts
@@ -0,0 +1,10 @@
+export interface HeadInfo {
+  title?: string;
+  description?: string;
+  ogImagePath?: string;
+  ogTitle?: string;
+  ogDesc?: string;
+  isCurrentPath?: boolean;
+  specPath?: string;
+  isNoIndex?: boolean;
+}
